3ff6e834e8bbe09a8e9ea2041395f4d7a7b7ae39
Saurabh <saurabhsingh4778698@gmail.com>
2021-05-18 17:05:58 +0530

    added notes title feature

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ let addBtn = document.getElementById("addBtn");
 showNotes();
 addBtn.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
+  let myobj={text:addTxt.value,
+             title:addTitle.value }
   if (addTxt.value != "") {
-    notesObj.push(addTxt.value);
+    notesObj.push(myobj);
   }
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
   //   console.log(notesObj);
   showNotes();
 });
@@ -30,8 +34,8 @@ function showNotes() {
           <div class="noteCard card my-2 mx-2" style="width: 18rem;">
 
             <div class="card-body">
-                <h5 class="card-title">Note ${index + 1}</h5>
-                <p class="card-text">${element}</p>
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text">${element.text}</p>
                 <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
             </div>
         
